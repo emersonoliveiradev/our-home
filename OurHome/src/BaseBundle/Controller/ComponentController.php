c4db1cb65663b9f51ff76405f0caf8cae28872a7
EmersonOliveiraCode <emersonoliveiracode@gmail.com>
2019-04-12 13:44:17 -0300

    Sobreescreveu a view do component

--- OurHome/src/BaseBundle/Controller/ComponentController.php
+++ OurHome/src/BaseBundle/Controller/ComponentController.php
@@ -1,25 +1,140 @@
 <?php
 
 namespace BaseBundle\Controller;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use BaseBundle\Entity\Component;
+use BaseBundle\Form\ComponentType;
 
-
-class ComponentController extends Controller {
-    
+/**
+ * Component controller.
+ *
+ * @Route("/component")
+ */
+class ComponentController extends Controller
+{
     /**
-     * @Route("/componentes", name="component_index")
-     * @Method("GET")
+     * Lists all Component entities.
      *
+     * @Route("/", name="component_index")
+     * @Method("GET")
      */
-    public function indexAction(){
-        return $this->render('component/index.html.twig');
+    public function indexAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $components = $em->getRepository('BaseBundle:Component')->findAll();
+
+        return $this->render('component/index.html.twig', array(
+            'components' => $components,
+        ));
     }
-    
+
+    /**
+     * Creates a new Component entity.
+     *
+     * @Route("/new", name="component_new")
+     * @Method({"GET", "POST"})
+     */
+    public function newAction(Request $request)
+    {
+        $component = new Component();
+        $form = $this->createForm('BaseBundle\Form\ComponentType', $component);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($component);
+            $em->flush();
+
+            return $this->redirectToRoute('component_show', array('id' => $component->getId()));
+        }
+
+        return $this->render('component/new.html.twig', array(
+            'component' => $component,
+            'form' => $form->createView(),
+        ));
+    }
+
+    /**
+     * Finds and displays a Component entity.
+     *
+     * @Route("/{id}", name="component_show")
+     * @Method("GET")
+     */
+    public function showAction(Component $component)
+    {
+        $deleteForm = $this->createDeleteForm($component);
+
+        return $this->render('component/show.html.twig', array(
+            'component' => $component,
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Displays a form to edit an existing Component entity.
+     *
+     * @Route("/{id}/edit", name="component_edit")
+     * @Method({"GET", "POST"})
+     */
+    public function editAction(Request $request, Component $component)
+    {
+        $deleteForm = $this->createDeleteForm($component);
+        $editForm = $this->createForm('BaseBundle\Form\ComponentType', $component);
+        $editForm->handleRequest($request);
+
+        if ($editForm->isSubmitted() && $editForm->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($component);
+            $em->flush();
+
+            return $this->redirectToRoute('component_edit', array('id' => $component->getId()));
+        }
+
+        return $this->render('component/edit.html.twig', array(
+            'component' => $component,
+            'edit_form' => $editForm->createView(),
+            'delete_form' => $deleteForm->createView(),
+        ));
+    }
+
+    /**
+     * Deletes a Component entity.
+     *
+     * @Route("/{id}", name="component_delete")
+     * @Method("DELETE")
+     */
+    public function deleteAction(Request $request, Component $component)
+    {
+        $form = $this->createDeleteForm($component);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($component);
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('component_index');
+    }
+
+    /**
+     * Creates a form to delete a Component entity.
+     *
+     * @param Component $component The Component entity
+     *
+     * @return \Symfony\Component\Form\Form The form
+     */
+    private function createDeleteForm(Component $component)
+    {
+        return $this->createFormBuilder()
+            ->setAction($this->generateUrl('component_delete', array('id' => $component->getId())))
+            ->setMethod('DELETE')
+            ->getForm()
+        ;
+    }
 }
-
-
-
-
---
+++ OurHome/src/BaseBundle/Entity/Component.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace BaseBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Component
+ *
+ * @ORM\Table(name="component")
+ * @ORM\Entity(repositoryClass="BaseBundle\Repository\ComponentRepository")
+ */
+class Component
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="title", type="string", length=255, unique=true)
+     */
+    private $title;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="description", type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @var bool
+     *
+     * @ORM\Column(name="status", type="boolean")
+     */
+    private $status;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return Component
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set description
+     *
+     * @param string $description
+     *
+     * @return Component
+     */
+    public function setDescription($description)
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    /**
+     * Get description
+     *
+     * @return string
+     */
+    public function getDescription()
+    {
+        return $this->description;
+    }
+
+    /**
+     * Set status
+     *
+     * @param boolean $status
+     *
+     * @return Component
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+
+    /**
+     * Get status
+     *
+     * @return bool
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+}
+
