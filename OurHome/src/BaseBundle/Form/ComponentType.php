a6afaa43b297fde36a143ebb1cf58c827ac0cc8e
EmersonOliveiraCode <emersonoliveiracode@gmail.com>
2019-04-12 18:01:43 -0300

    Documentacao

--- OurHome/src/BaseBundle/Form/ComponentType.php
+++ OurHome/src/BaseBundle/Form/ComponentType.php
@@ -1,33 +1,25 @@
 <?php
 
 namespace BaseBundle\Form;
-
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ComponentType extends AbstractType
-{
+
+class ComponentType extends AbstractType {
+    
     /**
      * @param FormBuilderInterface $builder
-     * @param array $options
+     * @param array                $options
      */
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder
-            ->add('title')
-            ->add('description')
-            ->add('status')
-        ;
+    public function buildForm(FormBuilderInterface $builder, array $options) {
+        $builder->add('title')->add('description')->add('status');
     }
     
     /**
      * @param OptionsResolver $resolver
      */
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults(array(
-            'data_class' => 'BaseBundle\Entity\Component'
-        ));
+    public function configureOptions(OptionsResolver $resolver) {
+        $resolver->setDefaults(array('data_class' => 'BaseBundle\Entity\Component'));
     }
 }
