60ab7454c3df4516f95939ce1a4e32706e0500a4
EmersonOliveiraCode <emersonoliveiracode@gmail.com>
2019-05-02 14:59:16 -0300

    Alterada a forma de criar os forms - falta pegar o id do usuário ativo

--- OurHome/src/BaseBundle/Form/ComponentType.php
+++ OurHome/src/BaseBundle/Form/ComponentType.php
@@ -4,24 +4,45 @@
 # Alterar modo de enviar label entity
 
 namespace BaseBundle\Form;
+use Doctrine\ORM\EntityRepository;
+use http\Client\Curl\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 
 
 class ComponentType extends AbstractType {
-    
+
     /**
      * @param FormBuilderInterface $builder
      * @param array                $options
      */
     public function buildForm(FormBuilderInterface $builder, array $options) {
+
         $builder
             ->add('title', null, array('label' => 'Nome'))
             ->add('description',null, array('label' => 'Descrição'))
             ->add('status',null, array('label' => 'Status'))
-            ->add('user',null, array('label' => 'Usuário'))
-            ->add('surrounding',null, array('label' => 'Ambiente'));
+
+            ->add('user',EntityType::class,[
+                'label' => 'Nome do usuário',
+                'placeholder' => 'Selecione o usuário',
+                'class' => 'BaseBundle\Entity\User',
+                'choice_label' => 'username',
+                'query_builder' => function (EntityRepository $er) {
+                    return $er->createQueryBuilder('u')
+                        ->where('u.id = :id')
+                        ->setParameter(':id', 1);
+                },
+            ])
+
+            ->add('surrounding',EntityType::class,[
+                'label' => 'Ambiente',
+                'placeholder' => 'Selecione um ambiente',
+                'class' => 'BaseBundle\Entity\Surrounding',
+                'choice_label' => 'title',
+            ]);
     }
     
     /**
--- OurHome/src/BaseBundle/Form/RegistrationFormType.php
+++ OurHome/src/BaseBundle/Form/RegistrationFormType.php
@@ -1,26 +1,26 @@
 <?php
 
 namespace BaseBundle\Form;
-use FOS\UserBundle\Util\LegacyFormHelper;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use FOS\UserBundle\Form\Type\RegistrationFormType as BaseRegistrationFormType;
-
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Coruja\BaseBundle\Form\Subscriber\UserSubscriber;
 
 
 
 class RegistrationFormType extends AbstractType {
-    
+
     public function buildForm(FormBuilderInterface $builder, array $options) {
         $builder
-            ->add('username', null, array('label' => 'Nome de acesso', 'translation_domain' => 'FOSUserBundle'))
-            ->add('email', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\EmailType'), array('label' => 'E-mail', 'translation_domain' => 'FOSUserBundle'))
-            ->add('plainPassword', LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\RepeatedType'), array(
-                'type' => LegacyFormHelper::getType('Symfony\Component\Form\Extension\Core\Type\PasswordType'),
-                'options' => array('translation_domain' => 'FOSUserBundle'),
+            ->add('username', null, array('label' => 'Nome de acesso'))
+            ->add('email', 'Symfony\Component\Form\Extension\Core\Type\EmailType', array('label' => 'E-mail', 'translation_domain' => 'FOSUserBundle'))
+            ->add('plainPassword', 'Symfony\Component\Form\Extension\Core\Type\RepeatedType', array(
+                'type' => 'Symfony\Component\Form\Extension\Core\Type\PasswordType',
                 'first_options' => array('label' => 'Senha'),
                 'second_options' => array('label' => 'Confirme a senha'),
-                'invalid_message' => 'fos_user.password.mismatch',
+                'invalid_message' => 'As senhas precisam ser iguais.',
             ))
         ;
     }
